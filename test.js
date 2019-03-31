33d1116c31a558c6aea832b888264b7bcc31a63d
Asad Saeeduddin <masaeedu@gmail.com>
2019-03-31 12:53:34 -0400

    Test more of the happy path

--- test.js
+++ test.js
@@ -6,23 +6,59 @@ const test = require("ava");
 const { pmatch } = require(".");
 
 const { Left, Right } = Either;
-const { Nothing, Just } = Maybe;
 
 const snap = t => x => t.snapshot(x);
 
 test("it works", t => {
-  const f = pmatch({
-    "_ (Left x)": ({ x }) => Left(x),
-    "f (Right x)": ({ f, x }) => f(x)
+  const Prim = { equals: x => y => x === y };
+
+  // :: Equatable e -> Equatable a -> Either e a -> Either e a -> Boolean
+  const equals = E => A =>
+    pmatch({
+      "(Left  _) (Right _)": _ => false,
+      "(Right _) (Left  _)": _ => false,
+      "(Left  x) (Left  y)": ({ x, y }) => E.equals(x)(y),
+      "(Right x) (Right y)": ({ x, y }) => A.equals(x)(y)
+    });
+
+  // Deliberately using pmatch unnecessarily here to
+  // test the implementation. For non-nested or non-nary
+  // matches, just using `match` is more convenient
+  // :: (a -> Either e b) -> Either e a -> Either e b
+  const chain = pmatch({
+    "_ (Left e)": ({ e }) => Left(e),
+    "f (Right a)": ({ f, a }) => f(a)
   });
 
+  // :: Either e (Either e a) -> Either e a
+  const join = pmatch({
+    "(Left e)": ({ e }) => Left(e),
+    "(Right (Right a))": ({ a }) => Right(a),
+    "(Right l)": ({ l }) => l
+  });
+
+  // :: (a -> b) -> Either e a -> Either e b
+  const map = pmatch({
+    "_ (Left e)": ({ e }) => Left(e),
+    "f (Right a)": ({ f, a }) => Right(f(a))
+  });
+
+  // :: (a -> Either e b) -> Either e a -> Either e b
+  const chain_ = f => ma => join(map(f)(ma));
+
+  // :: (a -> Boolean) -> a -> Either a a
   const guard = f => x => (f(x) ? Right(x) : Left(x));
 
-  const results = [
-    f(guard(x => x > 1))(Right(42)),
-    f(guard(x => x < 5))(Right(42)),
-    f(guard(x => x === 42))(Left("Whoops!"))
+  const inputs = [
+    [guard(x => x > 1), Right(42)],
+    [guard(x => x < 5), Right(42)],
+    [guard(x => x === 42), Left("Whoops!")]
   ];
 
-  results.forEach(snap(t));
+  inputs.forEach(([f, x]) => {
+    const v1 = chain(f)(x);
+    const v2 = chain_(f)(x);
+    t.true(equals(Prim)(Prim)(v1)(v2));
+    snap(t)(v1);
+  });
 });
