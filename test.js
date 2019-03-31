7efdc819e21ffb1e81045c0487da848e08a9c2cc
Asad Saeeduddin <masaeedu@gmail.com>
2019-03-31 02:26:28 -0400

    Use ava to add some tests

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
 const util = require("util");
-
 const { Maybe, Either } = require("@masaeedu/fp");
+
+const test = require("ava");
+
 const { pmatch } = require(".");
 
 const { Left, Right } = Either;
 const { Nothing, Just } = Maybe;
 
-const log = x => console.log(util.inspect(x, { depth: null }));
+const snap = t => x => t.snapshot(x);
 
-const maybechain = pmatch({
-  "_ (Left x)": ({ x }) => Left(x),
-  "f (Right x)": ({ f, x }) => f(x)
+test("it works", t => {
+  const f = pmatch({
+    "_ (Left x)": ({ x }) => Left(x),
+    "f (Right x)": ({ f, x }) => f(x)
+  });
+
+  const guard = f => x => (f(x) ? Right(x) : Left(x));
+
+  const results = [
+    f(guard(x => x > 1))(Right(42)),
+    f(guard(x => x < 5))(Right(42)),
+    f(guard(x => x === 42))(Left("Whoops!"))
+  ];
+
+  results.forEach(snap(t));
 });
-
-const results = maybechain(Maybe.of)(Right(42));
-
-log(results);
